3f69ce308a177abfb055dc179c51bda5d7ea7a44
Xangool <xandreale09@gmail.com>
2022-05-20 20:36:11 -0300

    Adiciona contagem do lançamento

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+
+  public dateNow = new Date();
+  public dDay = new Date('Jun 01 2022 00:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute  = 60;
+
+  public timeDifference: any;
+  public secondsToDday: any;
+  public minutesToDday: any;
+  public hoursToDday: any;
+  public daysToDday: any;
+
+
+  private getTimeDifference () {
+      this.timeDifference = this.dDay.getTime() - new  Date().getTime();
+      this.allocateTimeUnits(this.timeDifference);
+  }
+
+private allocateTimeUnits (timeDifference: any) {
+      this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+      this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+      this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+      this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+}
+
+ngOnInit() {
+    this.subscription = interval(1000).subscribe(x => { this.getTimeDifference(); });
+}
+
+ ngOnDestroy() {
+    this.subscription.unsubscribe();
+ }
+}
